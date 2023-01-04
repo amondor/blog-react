56484b6f7d2190154502839eff30fe756db3eab0
Andrew Mondor <mondor.andrew@gmail.com>
2023-01-04 14:02:38 +0100

    acceuil done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Containers/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar /> 
+      <Home />
       
     </div>
   );
