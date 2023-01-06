fecef78a36c508296dd8fa55c5a2c562d284f6b5
Andrew Mondor <mondor.andrew@gmail.com>
2023-01-06 17:16:32 +0100

    page contact done

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import './Contact.css'
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className="container-contact">
+      <h1>Contactez-nous</h1>
+      <p>Par mail : </p>     
+      <p>Par téléphone : </p>     
+      <p>Rejoignez-nous sur les réseaux </p>  
+      <ul>
+      <li>Facebook</li>
+      <li>Twitter</li>
+      <li>Youtube</li>
+    </ul>   
+    </div>
+   
   )
 }
